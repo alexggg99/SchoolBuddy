44cf1713d64bc573817a0ea35469683a3bcf879e
alexggg99 <alexggg99gmail.com>
2016-01-25 18:02:29 +0300

    edit modal window (debbuging)

--- src/main/resources/static/js/controllers/SchoolController.js
+++ src/main/resources/static/js/controllers/SchoolController.js
@@ -7,6 +7,7 @@ app.controller('SchoolController', function($scope, SchoolService){
     $scope.fields = ['id' ,'name', 'address', 'director'];
     $scope.school = {};
     $scope.show = function(school){
-        console.log(school);
+        $scope.school = school;
+        $('#editModal').modal('toggle');
     }
 })
